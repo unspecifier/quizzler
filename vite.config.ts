58fe281112830f083334e6e1de6b5fb9eec51a5b
cristina <cristinacolon24@aol.com>
2024-11-23 23:09:42 -0500

    add gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,5 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  base: "./",
-  build: {
-    outDir: "docs"
-  }
+  base: "/quizzler/",
 })
